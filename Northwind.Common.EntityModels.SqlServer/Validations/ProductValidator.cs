e101a903ccbaa1d4f07988d92ccb2d5e26bc8fd4
Abdelrhman Ahmed <64210458+abdooAhmed@users.noreply.github.com>
2024-06-14 16:36:48 +0300

    Add project files.

---
+++ Northwind.Common.EntityModels.SqlServer/Validations/ProductValidator.cs
@@ -0,0 +1,27 @@
+﻿using Northwind.EntityModels;
+
+namespace FluentValidation.Validators;
+
+public class ProductValidator : AbstractValidator<Product>
+{
+  public ProductValidator()
+  {
+    RuleFor(product => product.ProductId)
+      .NotEmpty(); // Not default(long) which is 0.
+
+    RuleFor(order => order.ProductName)
+      .NotNull()
+      .WithName("Name"); // Use Name instead of CustomerName in messages.
+
+   
+
+   
+
+    RuleFor(order => order.UnitPrice)
+      .GreaterThan(0);
+
+    
+
+    
+  }
+}
---
+++ Northwind.WebApi.Client.Mvc/Models/HomeSendMessageViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Northwind.WebApi.Client.Mvc.Models;
+
+public class HomeSendMessageViewModel
+{
+  public string? Info { get; set; }
+  public string? Error { get; set; }
+}
